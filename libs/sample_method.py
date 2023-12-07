8aa03a18c71ec8f1b80920fc66ae4b3ebec1581b
zzqGD32 <zhengqizhang229@gmail.com>
2023-12-07 15:14:53 +0800

    small cov init

--- libs/sample_method.py
+++ libs/sample_method.py
@@ -42,8 +42,8 @@ class AAISGaussian:
         count_add = self.count_dict['add']
         n = node.shape[0]
         num = int(np.ceil(n*0.1))
-        #cov_init = 1/(np.power(0.1*num, 2))
-        cov_init = 0.01
+        cov_init = 1/(np.power(0.1*num, 2))
+        #cov_init = 0.01
         # validity check
         if (len(ess_lad) != len(self.a_lad)) | (len(count_lad) != len(self.a_lad)):
             raise ValueError('invalid ladder please check')
